61cdb32308d89b3fcf5bbe37137fc1d2ac13e156
SheilaD <sheilahernandezdiaz@SheilaDev-Mac.attlocal.net>
2020-04-25 17:36:20 -0400

    blog master branch

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   resources :posts
+  resources :comments
+  # resources :posts do
+  #   get    '/comments',          to: 'comments#index', as: 'comments'
+  #   post   '/comments',          to: 'comments#create'
+  #   get    '/comments/new',      to: 'comments#new',   as: 'new_comment'
+  #   get    '/comments/:id',      to: 'comments#show',  as: 'comment'
+  #   get    '/comments/:id/edit', to: 'comments#edit',  as: 'edit_comment'
+  #   patch  '/comments/:id',      to: 'comments#update'
+  #   delete '/comments/:id',      to: 'comments#destroy'
+  # end
 
   # get '/pages', to: 'pages#index'
   # post '/pages', to: 'pages#create'
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200420222144) do
+ActiveRecord::Schema.define(version: 20200423025417) do
 
+  create_table "comments", force: :cascade do |t|
+    t.text     "content"
+    t.string   "name"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
+  create_table "owners", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "pages", force: :cascade do |t|
     t.string   "title"
