2d399c50a0f7022a06d74555a163d1a820a758e2
SheilaD <sheilahernandezdiaz@SheilaDev-Mac.attlocal.net>
2020-04-27 18:01:16 -0400

    Add updates to blog associations

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,5 @@
 Rails.application.routes.draw do
-  resources :posts
+  resources :posts do
   resources :comments
-  # resources :posts do
-  #   get    '/comments',          to: 'comments#index', as: 'comments'
-  #   post   '/comments',          to: 'comments#create'
-  #   get    '/comments/new',      to: 'comments#new',   as: 'new_comment'
-  #   get    '/comments/:id',      to: 'comments#show',  as: 'comment'
-  #   get    '/comments/:id/edit', to: 'comments#edit',  as: 'edit_comment'
-  #   patch  '/comments/:id',      to: 'comments#update'
-  #   delete '/comments/:id',      to: 'comments#destroy'
-  # end
-
-  # get '/pages', to: 'pages#index'
-  # post '/pages', to: 'pages#create'
-  # get '/pages/new', to: 'pages#new', as: 'new_page'
-  # get '/pages/:id', to: 'pages#show', as: 'page'
-  # get '/pages/:id/edit', to:'pages#edit', as: 'edit_page'
-  # patch '/pages/:id', to: 'pages#update'
-  # delete '/pages/:id', to: 'pages#destroy'
-  resources :pages
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
 end
